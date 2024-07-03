13897088d84c2a0d0965b30c51ae5b386757278a
Tonny Bett <betttonny26@gmail.com>
2024-07-03 19:06:36 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let message
+  if(feet <= 400){
+    message = 'This one is on me!'
+  }else if(feet>400 && feet<=2000){
+    message = 'That will be twenty bucks.';
+  }else if(feet>2000 && feet <=2500){
+    message = 'I will gladly take your thirty bucks.';
+  }else if(feet>2500){
+    message = 'No can do.';
+  }
+  return message;
+}
+function ternaryCheckCity(city){
+  return city==="NYC"? "Ok, sounds good." : "No go.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if(tip==='generous'){
+    return 'Thank you so much.';
+  }else if(tip==='not as generous'){
+    return 'Thank you.';
+  }else{
+    return 'Bye.';
+  }
 }
